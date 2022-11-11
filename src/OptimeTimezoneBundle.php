8e85b57afd40fbd0da13dd5c1750639333e954b3
optimeadmin <70289611+optimeadmin@users.noreply.github.com>
2022-11-11 10:25:39 -0500

    Create OptimeTimezoneBundle.php

---
+++ src/OptimeTimezoneBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Manuel Aguirre
+ */
+
+namespace Optime\Timezone\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Manuel Aguirre
+ */
+class OptimeTimezoneBundle extends Bundle
+{
+}
